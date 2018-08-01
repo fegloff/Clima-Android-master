d999a6bc7fe54990d8a79b612566eccc5472f78c
Francisco Egloff <fegloff@gmail.com>
2018-07-31 23:20:39 -0500

    Configurando el LocationManager

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,7 +1,12 @@
 package com.londonappbrewery.climapm;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,13 +17,14 @@ public class WeatherController extends AppCompatActivity {
     // Constants:
     final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
     // App ID to use OpenWeather data
-    final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
+    final String APP_ID = "3031f0e14e8d45bd0d4f8dce4f02eb5f";
     // Time between location updates (5000 milliseconds or 5 seconds)
     final long MIN_TIME = 5000;
     // Distance between location updates (1000m or 1km)
     final float MIN_DISTANCE = 1000;
 
     // TODO: Set LOCATION_PROVIDER here:
+    String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
 
 
 
@@ -29,7 +35,11 @@ public class WeatherController extends AppCompatActivity {
     TextView mTemperatureLabel;
 
     // TODO: Declare a LocationManager and a LocationListener here:
+    //Component who start or stop requesting location updates
+    LocationManager mLocationManager;
 
+    // component who will be notified if the location already change
+    LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +60,14 @@ public class WeatherController extends AppCompatActivity {
 
 
     // TODO: Add onResume() here:
-
+    @Override
+    // onResume its execute just after onCreate but before the user can interact with the activiry
+    protected void onResume() {
+        super.onResume();
+        Log.d("Clima","OnResume called");
+        Log.d("Clima","Getting weather for current location");
+        getWeatherForCurrentLocation();
+    }
 
 
     // TODO: Add getWeatherForNewCity(String city) here:
@@ -58,7 +75,32 @@ public class WeatherController extends AppCompatActivity {
 
 
     // TODO: Add getWeatherForCurrentLocation() here:
+    private void getWeatherForCurrentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+    }
 
 
     // TODO: Add letsDoSomeNetworking(RequestParams params) here:
